43bb515e57d5dc8d31fbd1f9f53255622b857a59
Mathew M <mathewmastromonaco@gmail.com>
2024-07-19 01:25:52 -0400

    added character counter logic.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  console.log('composer-char-counter.js is loaded');
+
+  
+  $('.new-tweet textarea').on('input', function() {
+    const maxLength = 140;
+    const currentLength = $(this).val().length;
+    const remainingCharacters = maxLength - currentLength;
+
+    const counterElement = $(this).siblings('div').find('.counter');
+    counterElement.text(remainingCharacters);
+
+    if (remainingCharacters < 0) {
+      counterElement.addClass('negative');
+    } else {
+      counterElement.removeClass('negative');
+    }
+  });
+});
